5c58f6099d39bfaa580e57eb95cfcc7b4f326f5f
jessiu2 <jessicaliu9130@gmail.com>
2023-10-25 12:00:22 +0200

    seance6

--- mini-projet/mini_projet_sort.sh
+++ mini-projet/mini_projet_sort.sh
@@ -1,13 +1,3 @@
-file="$1"
-count=1
-
-while IFS= read -r line; do
-    line_with_number=$(echo "$line" | sed -E 's|(https://)|'"$count"'\1|')
-    echo "$line_with_number"  
-    count=$((count+1))
-
-done < "$file"
-
     if [$# -ne 1]
   echo "Un argument attendu exactement"
   
