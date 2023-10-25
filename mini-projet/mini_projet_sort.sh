7d3930178119958f74ff91290f61c129a2a528eb
jessiu2 <jessicaliu9130@gmail.com>
2023-10-25 11:40:09 +0200

    seance6

--- mini-projet/mini_projet_sort.sh
+++ mini-projet/mini_projet_sort.sh
@@ -3,9 +3,9 @@ count=1
 
 while IFS= read -r line; do
     line_with_number=$(echo "$line" | sed -E 's|(https://)|'"$count"'\1|')
-    echo "$count $line_with_number"  
+    echo "$line_with_number"  
     count=$((count+1))
-    
+
 done < "$file"
 
     
