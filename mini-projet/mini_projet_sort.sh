910d34b1070c14600024d2c4453a069d225fe5a3
jessiu2 <jessicaliu9130@gmail.com>
2023-10-25 11:18:07 +0200

    seance6

---
+++ mini-projet/mini_projet_sort.sh
@@ -0,0 +1,11 @@
+file="$1"
+count=1
+
+while IFS= read -r line;
+do
+line_with_number=$(echo "$line" | sed -E 's|(https://)|'"$count"'\1|')
+echo ${line_with_number};
+count=$((count+1));
+
+done < "$file"
+    
