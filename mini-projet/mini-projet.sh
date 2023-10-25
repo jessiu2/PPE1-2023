6a8011c2052a0c4c863eaad6d4a161df520d2e2e
jessiu2 <jessicaliu9130@gmail.com>
2023-10-25 11:58:24 +0200

    seance6

--- mini-projet/mini-projet.sh
+++ mini-projet/mini-projet.sh
@@ -1,4 +1,20 @@
-file="$1"
+if [$# -ne 1]
+do
+  echo "Un argument attendu exactement"
+  
+  exit
+else
+   if [-f $1]
+   then 
+     echo "on a bien un fichier"
+else
+     echo "on attend un fichiers qui existe"
+     exit
+   fi
+fi
+
+
+file=$1
 
 while read -r line;
 do
--- mini-projet/mini_projet_sort.sh
+++ mini-projet/mini_projet_sort.sh
@@ -8,4 +8,27 @@ while IFS= read -r line; do
 
 done < "$file"
 
-    
+    if [$# -ne 1]
+do
+  echo "Un argument attendu exactement"
+  
+  exit
+else
+   if [-f $1]
+   then 
+     echo "on a bien un fichier"
+else
+     echo "on attend un fichiers qui existe"
+     exit
+   fi
+fi
+
+
+file=$1
+lineo=1
+
+while read -r line;
+do
+   echo -e "${lineno}\t${line}"
+   lineno=$(expr $lineno + 1)
+done < "$file";
